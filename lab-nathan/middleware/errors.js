b8d722952586fb30b31c4ea1b93a8f79fae22ee3
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-08-07 14:24:07 -0700

    Adds error middleware.

---
+++ lab-nathan/middleware/errors.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const createError = require('http-errors');
+
+const errors = function(error, request, response, next) {
+  if (error.name === 'ValidationError') {
+    error = createError(400, error.message);
+  }
+  
+  if (!error.status) {
+    error = createError(500, error.message);
+  }
+
+  response.status(error.status).send(error.name);
+  next();
+}
+
+module.exports = errors;
--- lab-nathan/server.js
+++ lab-nathan/server.js
@@ -6,12 +6,14 @@ const cors = require('cors');
 const morgan = require('morgan');
 const debug = require('debug')('basic-authorization-server:server');
 
+const errors = require('./middleware/errors.js');
 
 const app = express();
 dotenv.config();
 
 app.use(cors());
 app.use(morgan('dev'));
+app.use(errors);
 
 app.listen(process.env.PORT, function() {
   debug(`Listening on port ${process.env.PORT}.`);
