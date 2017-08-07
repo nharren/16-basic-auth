e26e2b4df669151210ed381ebeace2b152a2f228
Nathan Harrenstein <nathan.harrenstein@gmail.com>
2017-08-07 13:50:43 -0700

    Adds cors and morgan.

--- lab-nathan/server.js
+++ lab-nathan/server.js
@@ -2,10 +2,16 @@
 
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
+const morgan = require('morgan');
 const debug = require('debug')('basic-authorization-server:server');
+
 
 const app = express();
 dotenv.config();
+
+app.use(cors());
+app.use(morgan('dev'));
 
 app.listen(process.env.PORT, function() {
   debug(`Listening on port ${process.env.PORT}.`);
